39cadf7daac59d1f49fd7c46e5242b79c51f9669
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-12-02 15:21:37 -0400

    Implementação das anotações do JPA nas tabelas

--- src/main/kotlin/dev/jrge/request/credit/system/models/Credit.kt
+++ src/main/kotlin/dev/jrge/request/credit/system/models/Credit.kt
@@ -1,15 +1,17 @@
 package dev.jrge.request.credit.system.models
 
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
 data class Credit(
-    val id: Long,
-    val creditCode :UUID  = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallment: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null
-    )
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallment: Int = 0,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null
+)
--- src/main/kotlin/dev/jrge/request/credit/system/models/Customer.kt
+++ src/main/kotlin/dev/jrge/request/credit/system/models/Customer.kt
@@ -1,15 +1,20 @@
 package dev.jrge.request.credit.system.models
 
+import jakarta.persistence.*
 import java.math.BigDecimal
 
+@Entity
 data class Customer(
-    val id: Long? = null,
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String = "",
-    var email: String = "",
-    var password: String = "",
-    var address: Address= Address(),
-    var icome: BigDecimal = BigDecimal.ZERO,
-    var credits: List<Credit> = mutableListOf()
-    )
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String = "",
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) var icome: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = arrayOf(CascadeType.REMOVE), mappedBy = "customer"
+    ) var credits: List<Credit> = mutableListOf()
+)
